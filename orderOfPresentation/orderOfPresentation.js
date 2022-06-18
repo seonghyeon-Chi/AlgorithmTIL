a7189aee38efedfd1ac931354e62808fd92db33c
seonghyeon-Chi <jiji6027@gmail.com>
2022-06-18 15:21:07 +0900

    [미완료]orderOfPresentation

--- orderOfPresentation/orderOfPresentation.js
+++ orderOfPresentation/orderOfPresentation.js
@@ -1,3 +1,12 @@
 function orderOfPresentation (N, K) {
-  // TODO: 여기에 코드를 작성합니다.
+  // TODO: 배열의 경우의 수를 구하는 문제
+  // 첫번째 배열, 두번째 배열, 세번째 배열의 분리해서 움직이는 경우로 한다.
+  // N에 맞게 1부터 N번까지 순차적인 1차원 배열 생성한다
+  let array = []
+  for (let i = 1; i <= 3; i++) {
+    array.push(i)
+  }
+  let result = [array];
+  let K = 1
+  let i = 0
 }
