e1e2342aeece0be3b87dba68422921f36616c11d
seonghyeon-Chi <jiji6027@gmail.com>
2022-07-06 17:56:00 +0900

    [미완료]power 2

--- power/power.js
+++ power/power.js
@@ -1,11 +1,15 @@
 function power(base, exponent) {
   // todo: 재귀를 사용해서 제곱을 구한다
+  // 시간 복잡도를 위해 분할 정복을 사용한다
   if (exponent === 0) {
     return 1
+  } else if (exponent % 2 === 0) {
+    return power(base, exponent/2) * power(base, exponent/2)
   } else {
-    return base * power(base, exponent-1)
+    return base * power(base, (exponent-1)/2) * power(base, (exponent-1)/2)
   }
 }
+
 
 // 수가 94,906,249가 넘는 경우는 제대로 된 정답이 나오지 않는다
 // 결과를 94,906,249로 나누는 것이 아니라 94,906,249보다 넘는 경우에 나눠서 연산을 이어가야 한다.
