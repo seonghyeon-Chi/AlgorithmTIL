8d60487858e571ffc3a6055989154ec2785316a9
seonghyeon-Chi <jiji6027@gmail.com>
2022-07-19 22:24:44 +0900

    binarySearch

--- binarySearch/binarySearch.js
+++ binarySearch/binarySearch.js
@@ -3,25 +3,20 @@ const binarySearch = function (arr, target) {
   // X가 중간 값보다 작으면 중간 값을 기준으로 좌측의 데이터들을 대상으로
   // X가 중간값보다 크면 배열의 우측을 대상으로 다시 탐색한다.
   // 해당 값을 찾을 때까지 이 과정을 반복한다.
-  let index = 0;
-  function search (arr, target) {
-    if (arr.length === 0) {
-      return -1
-    }
-    let half = Math.floor(arr.length / 2)
-    if (arr[half] === target) {
-      index += half
-      return index
+  let left = 0;
+  let right = arr.length - 1
+
+  while (left <= right) {
+    let middle = Math.floor((right + left) / 2)
+    if (arr[middle] === target) {
+      return middle
     } else {
-      if (target < arr[half]) {
-        return search(arr.slice(0,half), target)
+      if (arr[middle] < target) {
+        left = middle + 1
       } else {
-        index += arr.slice(0,half+1).length
-        return search(arr.slice(half+1), target)
+        right = middle - 1
       }
     }
   }
-  return search(arr, target)
+  return -1
 };
-
-// console에서는 문제 없이 작동되나 테스트에서는 통과되지 않음
